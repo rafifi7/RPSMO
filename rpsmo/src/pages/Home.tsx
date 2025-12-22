18d42b9496b9995c95405bf6a51e480b7b2f8198
rafifi7 <79894190+rafifi7@users.noreply.github.com>
2025-12-22 05:24:34 -0500

    styling home page

--- rpsmo/src/pages/Home.tsx
+++ rpsmo/src/pages/Home.tsx
@@ -9,6 +9,8 @@ const Home: React.FC = () => {
         <div className="min-h-screen flex flex-col bg-gray-900 border-l border-r border-sg-pink">
             <div className="flex flex-col items-center py-16 px-10  bg-gray-900 mx-4">
                 <main className="flex-grow flex flex-col items-center justify-center w-full max-w-2xl">
+
+                    
                     <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold text-sg-pink tracking-wider mb-32 whitespace-nowrap">
                         Rock Paper Scissors Minus One
                     </h1>
